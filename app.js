9f259a9b4cd12a4653acf9788a6b465b739c3fb3
ronyahmmod <rony.jib@gmail.com>
2020-09-17 23:43:47 +0600

    Add hash functionality to my app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 // config dotenv to my config file
 dotenv.config({ path: './config.env' });
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
 		required: true
 	}
 });
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: [ 'password' ] });
+
 const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
@@ -45,7 +45,7 @@ app.post('/signup', async (req, res) => {
 		const password = req.body.register_password;
 		const password_a = req.body.register_password_a;
 		if (password !== password_a) throw new Error('Password and Confirm password is not matched');
-		const newUser = await User.create({ email: email, password: password_a });
+		const newUser = await User.create({ email: email, password: md5(password) });
 		console.log(newUser);
 		res.status(200).render('secret', { title: 'Secret Page' });
 	} catch (err) {
@@ -58,7 +58,7 @@ app.post('/signin', async (req, res) => {
 	try {
 		const email = req.body.login_email;
 		console.log(email);
-		const password = req.body.login_password;
+		const password = md5(req.body.login_password);
 
 		const user = await User.findOne({ email: email });
 		if (!user) {
